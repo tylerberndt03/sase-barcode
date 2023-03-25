d6ae67293d26581a95cc8accb3e78bf64f6b3e72
Tyler B <101296374+tylerberndt03@users.noreply.github.com>
2023-03-25 16:54:29 -0400

    Add files via upload

---
+++ db-to-sheet.py
@@ -0,0 +1,28 @@
+import sqlalchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Member(Base):
+    __tablename__ = "members"
+
+    barcode_id: Mapped[int] = mapped_column(primary_key=True)
+    ufid: Mapped[int]
+    name: Mapped[str]
+    major: Mapped[str]
+
+    def __repr__(self) -> str:
+        return f"Member(barcode_id={self.barcode_id!r}, ufid={self.ufid!r}, name={self.name!r}, major={self.major!r})"
+
+def AcquireFromDB(barcode_id) -> Member:
+    pass
+
+def AddToSheet(member: Member):
+    
