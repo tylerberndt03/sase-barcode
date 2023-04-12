63ebf0d6a2b0df03dc40d19226f0878bbef89ac4
tylerberndt03 <101296374+tylerberndt03@users.noreply.github.com>
2023-04-12 12:43:55 -0400

    Update barcode.py

--- barcode.py
+++ barcode.py
@@ -1,5 +1,15 @@
-#import 
-import picam
+import cv2
+from pyzbar.pyzbar import decode
 
 if __name__ == "__main__":
-    
+    cam = cv2.VideoCapture(0)
+
+    while True: # /shrug
+        ret, img = cam.read()
+        k = cv2.waitKey(1)
+        if k%256 == 27:
+            break
+        elif k%256 == 32:
+            barcode = decode(img)
+            print(barcode)
+            # later on I'll have the
