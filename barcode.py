f858cb2f1ef12c265b811ea801c3618a65c4a285
tylerberndt03 <101296374+tylerberndt03@users.noreply.github.com>
2023-04-20 18:01:46 -0400

    Update barcode.py

--- barcode.py
+++ barcode.py
@@ -4,26 +4,31 @@ import gspread
 import zxingcpp
 import tkinter as tk
 
+# ret, img = cam.read()
+#         #cv2.imshow("img", img)
+#         k = cv2.waitKey(1)
+#         if k%256 == 27:
+#            break
+#         elif k%256 == 32:
+#            barcode = zxingcpp.read_barcodes(img)
+#            print(barcode)
+#             later on I'll have the
+           
 def scanBarcode():
-        #ret, img = cam.read()
+        ret, img = cam.read()
         #cv2.imshow("img", img)
-        #k = cv2.waitKey(1)
-        #if k%256 == 27:
-        #    break
-        #elif k%256 == 32:
-        #    barcode = zxingcpp.read_barcodes(img)
-        #    print(barcode)
-            # later on I'll have the
+        barcode = zxingcpp.read_barcodes(img)
+        print(barcode)
         barcode = getBarcode.get()
         try:
             row = memberSheet.col_values(1).index(barcode) + 1
         except ValueError:
             print("No info found")
-            status.set("No info found")
+            status.set(barcode + ":\nNo info found")
         else:
             member = memberSheet.row_values(row)
             print("Info found: " + member[1])
-            status.set("Info found: " + member[1])
+            status.set(":\nInfo found: " + member[1])
             eventSheet.append_row(member)
 
 if __name__ == "__main__":
@@ -33,8 +38,10 @@ if __name__ == "__main__":
     memberSheet = gc.open("Members").get_worksheet(0) # or whatever it's called
     eventSheet = gc.open(eventSheetN).get_worksheet(0)
 
-    #cam = cv2.VideoCapture(0)
-    #cv2.namedWindow("img")
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow("img")
+    #Tescv2.imshow("img", img)
+
     window = tk.Tk()
     captureImg = tk.Button(text="Scan", command=scanBarcode)
     status = tk.StringVar(value="Waiting")
